6701df8c0e2324b0e9ef42efb51dfb3e5d20373c
Tom Ross <tom.ross@protonmail.com>
2021-09-24 16:59:54 +0800

    [UPD] Expand parser for multiple args

--- ross_thomas_18342884/src/signage_building.py
+++ ross_thomas_18342884/src/signage_building.py
@@ -13,18 +13,31 @@ def recognition(img):
 
 def read_input():
     parser = argparse.ArgumentParser()
-    parser.add_argument("dir")
-    args = parser.parse_args()
+    parser.add_argument("-i", "--input", required=True,
+                        help="directory with input images")
+    parser.add_argument("-w", "--work", required=True,
+                        help="directory for work images")
+    parser.add_argument("-o", "--output", required=True,
+                        help="directory for output images and data")
 
-    dir = args["dir"]
-    img_files = [f for f in os.listdir(dir) if os.isfile(f)]
+    args = vars(parser.parse_args())
 
-    return img_files
+    dir_input = args["i"]
 
-def main():
-    img_files = read_input()
+    files = [f for f in os.listdir(dir_input)
+             if os.path.isfile(os.path.join(dir_input, f))]
 
-    for img_file in img_files:
-        img = cv2.imread(img_file, cv2.IMREAD_COLOR)
-        root, ext = os.path.splitext(img_file)
-        print(f"{img_file} -> ({root}, {ext})")
+    img_files = [os.path.join(dir_input, f)
+                 for f in os.listdir(dir_input)
+                 if os.path.isfile(os.path.join(dir_input, f))
+                 and os.path.splitext(f)[1] == ".jpg"]
+
+    return args, img_files
+
+# program
+args, img_files = read_input()
+
+for img_file in img_files:
+    img = cv2.imread(img_file, cv2.IMREAD_COLOR)
+    root, ext = os.path.splitext(os.path.basename(img_file))
+    print(f"{img_file} -> ({root}, {ext})")
