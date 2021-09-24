3e8e36c0a3cb14fb8da0542766f2c1493048de7e
Tom Ross <tom.ross@protonmail.com>
2021-09-24 13:26:02 +0800

    [UPD] Adjust flag variable names

--- ross_thomas_18342884/common.sh
+++ ross_thomas_18342884/common.sh
@@ -6,17 +6,17 @@ set -o nounset
 set -o pipefail
 
 # Script messaging.
-FLAG_DEBUG="1"
-FLAG_ERROR="1"
+FLAG_MSG_DEBUG="1"
+FLAG_MSG_ERROR="1"
 
 function __msg_debug() {
-  if [ "${FLAG_DEBUG}" == "1" ] ; then
+  if [ "${FLAG_MSG_DEBUG}" == "1" ] ; then
     echo -e "[DEBUG] $*"
   fi
 }
 
 function __msg_error() {
-  if [ "${FLAG_ERROR}" == "1" ] ; then
+  if [ "${FLAG_MSG_ERROR}" == "1" ] ; then
     echo -e "[ERROR] $*"
   fi
 }
