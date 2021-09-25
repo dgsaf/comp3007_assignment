1678be32eca212e99637ea5e10681ce26eb4e819
Tom Ross <tom.ross@protonmail.com>
2021-09-25 14:39:38 +0800

    [UPD] Debug previous commit

--- ross_thomas_18342884/src/detect.py
+++ ross_thomas_18342884/src/detect.py
@@ -4,18 +4,17 @@ import os
 import numpy as np
 import cv2
 
-def background(img, n, k):
+def background(img, k, iterations):
     img_bg = img.copy()
-    for i in range(n):
-        # note: must have k - 2*n >= 1
-        img_bg = cv2.medianBlur(img_bg, k - (2*i))
+    for i in range(iterations):
+        img_bg = cv2.medianBlur(img_bg, k)
     return img_bg
 
-def salient(img, n, k):
-    img_bg = background(img, n, k)
+def salient(img, k, iterations):
+    img_bg = background(img, k, iterations)
     img_fg = cv2.absdiff(img, img_bg)
 
     img_fg_norm = img_fg.copy()
-    cv2.normalise(img_fg, img_fg_norm, alpha=0, beta=255,
+    cv2.normalize(img_fg, img_fg_norm, alpha=0, beta=255,
                   norm_type=cv2.NORM_MINMAX)
     return img_fg_norm
