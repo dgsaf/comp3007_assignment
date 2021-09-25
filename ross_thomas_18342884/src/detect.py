40023720a7deeb22a4698a0d3221aa3ebf9b6e0c
Tom Ross <tom.ross@protonmail.com>
2021-09-25 14:11:24 +0800

    [UPD] Implement preprocessing salient detection

--- ross_thomas_18342884/src/detect.py
+++ ross_thomas_18342884/src/detect.py
@@ -3,3 +3,19 @@
 import os
 import numpy as np
 import cv2
+
+def background(img, n, k):
+    img_bg = img.copy()
+    for i in range(n):
+        # note: must have k - 2*n >= 1
+        img_bg = cv2.medianBlur(img_bg, k - (2*i))
+    return img_bg
+
+def salient(img, n, k):
+    img_bg = background(img, n, k)
+    img_fg = cv2.absdiff(img, img_bg)
+
+    img_fg_norm = img_fg.copy()
+    cv2.normalise(img_fg, img_fg_norm, alpha=0, beta=255,
+                  norm_type=cv2.NORM_MINMAX)
+    return img_fg_norm
