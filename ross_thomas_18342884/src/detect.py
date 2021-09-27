68c9e4afc2dea7927b4455e8dde8ae13e28b6800
Tom Ross <tom.ross@protonmail.com>
2021-09-27 17:06:29 +0800

    [UPD] Fine tune parameters

--- ross_thomas_18342884/src/detect.py
+++ ross_thomas_18342884/src/detect.py
@@ -33,7 +33,7 @@ def binary_gradient_internal(img):
     edge_k = 3
     edge_iterations = 5
 
-    bin_k = 15
+    bin_k = 31
     bin_c = -15
 
     img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
@@ -51,13 +51,13 @@ def binary_gradient_external(img):
     edge_k = 3
     edge_iterations = 2
 
-    bin_k = 15
+    bin_k = 31
     bin_c = -15
 
     img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     img_blur = cv2.bilateralFilter(img_gray, blur_k, blur_sigma_c, blur_sigma_s)
     img_edge = edge_gradient_external(img_blur, edge_k, edge_iterations)
-    img_bin = binarize(img_edge, bin_k, bin_c)
+    img_bin = invert(binarize(img_edge, bin_k, bin_c))
     return img_bin
 
 
@@ -69,7 +69,7 @@ def binary_region(img):
     edge_k = 3
     edge_iterations = 2
 
-    bin_k = 15
+    bin_k = 31
     bin_c = -15
 
     img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
