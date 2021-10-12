aa37aa01b75b3394b3c60630eeafa0aa56deea9d
Tom Ross <tom.ross@protonmail.com>
2021-10-12 13:19:20 +0800

    [UPD] Debug previous commit

--- ross_thomas_18342884/task1.sh
+++ ross_thomas_18342884/task1.sh
@@ -1,29 +1,7 @@
 #!/bin/bash
 
-# Improve script safety.
-set -o errexit
-set -o nounset
-set -o pipefail
-
-# Script messaging.
-FLAG_MSG_DEBUG="1"
-FLAG_MSG_ERROR="1"
-
-function __msg_debug() {
-  if [ "${FLAG_MSG_DEBUG}" == "1" ] ; then
-    echo -e "[DEBUG] $*"
-  fi
-}
-
-function __msg_error() {
-  if [ "${FLAG_MSG_ERROR}" == "1" ] ; then
-    echo -e "[ERROR] $*"
-  fi
-}
-
 # Initialise top directory.
-DIR_TOP="/home/student"
-DIR_TOP="$(dirname "${PWD}")"
+DIR_TOP="/home/student/Documents/COMP3007/comp3007_assignment" # "/home/student"
 
 # Initialise directory variables.
 DIR_SUBMISSION="${DIR_TOP}/ross_thomas_18342884"
@@ -33,6 +11,9 @@ DIR_TEST="${DIR_TOP}/test/task1"
 DIR_VAL="${DIR_TOP}/val/task1"
 DIR_OUTPUT="${DIR_SUBMISSION}/output/task1"
 DIR_WORK="${DIR_SUBMISSION}/work"
+
+# Source common scripting functionality.
+source "${DIR_SUBMISSION}/common.sh"
 
 # Log directory variables.
 __msg_debug "Directories:"
