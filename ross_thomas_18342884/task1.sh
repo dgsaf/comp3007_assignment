0f40e640d6d482e245cadc58a8dc54cb48cba52b
Tom Ross <tom.ross@protonmail.com>
2021-10-12 12:44:40 +0800

    [UPD] Adjust task1.sh

--- ross_thomas_18342884/task1.sh
+++ ross_thomas_18342884/task1.sh
@@ -9,13 +9,17 @@ DIR_TOP="$(dirname "${PWD}")"
 
 # Initialise directory variables.
 DIR_SUBMISSION="${DIR_TOP}/ross_thomas_18342884"
+DIR_DIGITS="${DIR_TOP}/train/digits"
+DIR_WORK="${DIR_SUBMISSION}/work"
+
 DIR_TRAIN="${DIR_TOP}/train/task1"
 DIR_TEST="${DIR_TOP}/test/task1"
 DIR_VAL="${DIR_TOP}/val/task1"
+DIR_OUTPUT="${DIR_SUBMISSION}/output/task1"
 
 # Log directory variables.
 __msg_debug "Directories:"
-for name in TOP SUBMISSION TRAIN TEST VAL ; do
+for name in TOP SUBMISSION DIGITS WORK TRAIN TEST VAL OUTPUT ; do
   dir="DIR_${name}"
 
   if [ -d "${!dir}" ] ; then
@@ -25,3 +29,9 @@ for name in TOP SUBMISSION TRAIN TEST VAL ; do
     exit 1
   fi
 done
+
+python "${DIR_SUBMISSION}/src/task_1.py" \
+       -i "${DIR_TRAIN}" \
+       -o "${DIR_OUTPUT}" \
+       -d "${DIR_DIGITS}" \
+       -w "${DIR_WORK}" -W
